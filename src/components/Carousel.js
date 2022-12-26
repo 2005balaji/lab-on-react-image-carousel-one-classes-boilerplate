7ae270261d0bf0ae0afbe4ef56400970bda1a007
Balaji S <balajisr000@gmail.com>
2022-12-27 00:11:32 +0530

    Add existing project files to Git

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,13 +1,61 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Carousel.css";
-import { images } from "../data/CarouselData";
-// you can explore more - and check as how to use materiul ui
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { images } from '../data/CarouselData'
+// // icons from material UI
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
-// implement the class below
-class Carousel extends Component {
-  
+
+class Carousel extends React.Component {
+
+    constructor(props){
+        super(props)
+        
+        this.state={
+        currImg:0,
+        }
+        }
+        
+
+
+render(){
+
+
+    return(
+
+        <div className = 'carousel' >
+                <div className="carouselInner"
+                 style={{ backgroundImage: `url(${images[this.state.currImg].img})` }}>
+
+
+                    <div className="left" onClick={()=> { if( this.state.currImg>0 ) this.setState({currImg:this.state.currImg-1})}}>
+                    <ArrowBackIosIcon style={{ fontSize: 40 }} />
+                    </div>
+                   
+                    <div className="centrediv" id="centrediv">
+                        <h1>{images[this.state.currImg].title}</h1>
+                        <br /><br /><br /><br /><br /><br /><br /><br />
+                        <br /><br />
+                        <p>{images[this.state.currImg].subtitle}</p>
+                    </div>
+
+                    <div className="right" onClick={()=> { if (this.state.currImg< images.length - 1) this.setState({currImg:this.state.currImg+1})}}>
+                    <ArrowForwardIosIcon style={{ fontSize: 40 }} />
+                    </div>
+                        
+    
+                </div>
+    
+        </div>
+    )
+
 }
+    
+
+
+
+}
+
+
 
 export default Carousel;
